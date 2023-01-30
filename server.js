3f9b60b238ea06804722068aeca5906082a1ffa2
SpectreKilo <johnho8788@gmail.com>
2023-01-29 19:48:10 -0500

    delete function working?

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ const noteData = require("./db/db.json")
 
 const app = express();
 
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination} 👾`)
+  );
+
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -31,9 +36,21 @@ app.post("/api/notes", (req, res) => {
 });
 
 app.delete("/api/notes/:id", (req, res) => {
-    const id = req.params.id;
-    res.unlink(id, "./db/db.json")
-    res.readFile(id, "./db/db.json")
+    let deleteNoteId = req.params.id
+    fs.readFile("./db/db.json", function(err, data) {
+        if(err){
+            console.log(err);
+        }else{
+            var noteArr = JSON.parse(data)
+            var newArr = noteArr.filter(note => note.id !== deleteNoteId);
+            var strNewArr = JSON.stringify(newArr);
+            const updateNotes = writeToFile("./db/db.json", strNewArr);
+            res.send(updateNotes);
+        }
+    });
+    // const id = req.params.id;
+    // res.unlink(id, "./db/db.json")
+    // res.readFile(id, "./db/db.json")
 });
 
 app.get("*", (req, res) => 
