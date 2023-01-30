0799181106a9ea9df7111dc95c35bd58ed081e49
SpectreKilo <johnho8788@gmail.com>
2023-01-29 20:20:44 -0500

    trying to get delete function to work but i broke all my code and had to recopy my code from a previous iteration of server.js and db.json

--- server.js
+++ server.js
@@ -3,16 +3,10 @@ const path = require("path");
 const fs = require("fs");
 const util = require("util");
 const uniqid = require("uniqid");
-const PORT = 3001;
-// const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;
 const noteData = require("./db/db.json")
 
 const app = express();
-
-// const writeToFile = (destination, content) =>
-//   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-//     err ? console.error(err) : console.info(`\nData written to ${destination} 👾`)
-//   );
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -35,23 +29,10 @@ app.post("/api/notes", (req, res) => {
     })
 });
 
-app.delete("/api/notes/:id", (req, res) => {
-    // let deleteNoteId = req.params.id
-    // fs.readFile("./db/db.json", function(err, data) {
-    //     if(err){
-    //         console.log(err);
-    //     }else{
-    //         let noteArr = JSON.parse(data)
-    //         console.log(noteArr);
-    //         let newArr = noteArr.filter(note => note.id !== deleteNoteId);
-    //         let strNewArr = JSON.stringify(newArr);
-    //         const updateNotes = writeToFile("./db/db.json", strNewArr);
-    //         res.send(updateNotes);
-    //     }
-    // });
-    const id = req.params.id;
-    res.unlink(id, "./db/db.json")
-    res.readFile(id, "./db/db.json")
+app.delete("api/notes/:id", (req, res) => {
+    // const id = req.params.id;
+    // res.unlink(id, "./db/db.json")
+    // res.readFile(id, "./db/db.json")
 });
 
 app.get("*", (req, res) => 
